7fab923728774d79753d59efff615a301cb717a8
Sonia Sibaja <ssibaja@Sonias-MacBook-Pro.local>
2017-06-01 17:18:51 -0600

    Add the Factory implementation.

--- src/main/java/com/gap/atpractice/testSuites/NewEmployeeTest.java
+++ src/main/java/com/gap/atpractice/testSuites/NewEmployeeTest.java
@@ -11,6 +11,13 @@ import org.testng.annotations.Test;
  */
 public class NewEmployeeTest extends TestBase {
 
+    //Added this variable to handle the the iterations handled by the testFactory
+    private int testIndex;
+
+    //In this case the index is initializes the index.
+    public NewEmployeeTest (int index){
+        testIndex = index;
+    }
 
 
 //    @Test(groups = "newEmployee", dataProvider = "dp", dataProviderClass = NewEmployeeDataProvider.class, dependsOnGroups = {"loginSuccess"})
@@ -19,13 +26,14 @@ public class NewEmployeeTest extends TestBase {
                                 String employeeID, String employeeLeaderName, String year, String month,
                                 String day){
 
+
         // I need to call the Login test case first, just to login. But I need to refactor this later
         LoginPage loginpage = (LoginPage) new LoginPage(driver).get();
         loginpage.userLoginSuccess("at_java_training@wearegap.com", "123queso");
 
         //when using the last "get()", I'm avoiding the goToLoginPage method, It works because of the implementation of the Loadable component
         NewEmployeePage newEmployeePage = (NewEmployeePage) new NewEmployeePage(driver).get();
-        newEmployeePage.createNewEmployeeSuccess(firstEmployeeName,lastEmployeeName,employeeEmail,employeeID,
+        newEmployeePage.createNewEmployeeSuccess(firstEmployeeName,lastEmployeeName,employeeEmail,String.format("%s%d",employeeID,testIndex),
                                                 employeeLeaderName,year,month,day);
     }
 }
